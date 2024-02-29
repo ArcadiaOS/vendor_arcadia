39b831ab81b6a3deef89977c2b9808c18131790d
sirNewbies <yusromi04@gmail.com>
2024-03-01 01:51:19 +0700

    Add cmopleted build banner

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -26,4 +26,19 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/lineage/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(LINEAGE_VERSION).zip
 	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
-	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
+	@echo -e ${CL_GRN}""${CL_GRN}
+	@echo -e ${CL_GRN}"---------------------------------------------------"${CL_GRN}
+	@echo -e ${CL_GRN}"-   ___                    _ _       ____  _____  -"${CL_GRN}
+	@echo -e ${CL_GRN}"-  / _ \                  | (_)     /  _ \/  ___| -"${CL_GRN}
+	@echo -e ${CL_GRN}"- / /_\ \_ __ ___ __ _  __| |_  __ _| | | \ `--.  -"${CL_GRN}
+	@echo -e ${CL_GRN}"- |  _  | '__/ __/ _` |/ _` | |/ _` | | | |`--. \ -"${CL_GRN}
+	@echo -e ${CL_GRN}"- | | | | | | /_| |_| | /_| | | |_| \ \_/ /\__/ / -"${CL_GRN}
+	@echo -e ${CL_GRN}"- \_| |_/_|  \___\__,_|\__,_|_|\__,_|\___/\____/  -"${CL_GRN}
+	@echo -e ${CL_GRN}"---------------------------------------------------"${CL_GRN}
+	@echo -e ${CL_GRN}""${CL_GRN}
+	@echo -e ${CL_GRN}"================-Package Completed-================"${CL_RST}
+	@echo -e ${CL_GRN}"Zip: "${CL_YLW} $(LINEAGE_TARGET_PACKAGE) ${CL_RST}
+	@echo -e ${CL_GRN}"SHA256: "${CL_YLW}" `sha256sum $(LINEAGE_TARGET_PACKAGE) | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_GRN}"Size: "${CL_YLW}" `ls -l $(LINEAGE_TARGET_PACKAGE) | cut -d ' ' -f 5` "${CL_RST}
+	@echo -e ${CL_GRN}"==================================================="${CL_RST}
+	@echo -e ""	
